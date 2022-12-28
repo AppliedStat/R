ae31430f4429baf50f87dadfe3c3a0936592a31f
AppliedStat <cpark2@gmail.com>
2022-12-28 13:04:23 +0900

    version 1.22.12

--- weibullness/R/weibull.estimate.R
+++ weibullness/R/weibull.estimate.R
@@ -27,17 +27,32 @@ function(x, threshold, interval, interval.threshold, extendInt="downX",
 }
 #------------------------------
 # We need n when the data are right-censored at the max. obs
-weibull.wp <- function(x,n, a=0.5){ 
+weibull.wp <- function(x,n, a){   ## It was a=0.5
   x = sort(x)
   r = length(x)
   if ( missing(n) ) n=r
-  if ( n < r ) stop("n can not be smaller than the number of observations")
+  if (missing(a)) { a = ifelse(n <= 10, 3/8, 1/2) }
+  if ( n < r ) stop("n cannot be smaller than the number of observations")
   R = 1-ppoints(n,a=a)
   LM=lm(log(-log( R[1:r] ))~log(x))
   B = as.numeric( coef (LM) )
   alpha = B[2]
   beta  = exp(-B[1]/B[2])
   structure(list(shape=alpha,scale=beta), class="weibull.estimate")
+}
+#------------------------------
+weibull.rm <- function(x, a){   ## Repeated median (added on 2022-07-10)
+  x = sort( log(unique(x)) )
+  n = length(x)
+  if (missing(a)) { a = ifelse(n <= 10, 3/8, 1/2) }
+  y = log(-log(1-ppoints(n, a=a)))
+  DX = outer(x,x,"-"); DY = outer(y,y,"-")
+  diag(DX) = NA
+  alpha.hat = median( apply(DY/DX,2, median, na.rm=TRUE) )  # slope 
+  intercept = median(y-alpha.hat*x) # This intercept is better than the below. 
+  ## intercept = median( apply((outer(x,y,"*")-outer(y,x,"*"))/DX, 2, median, na.rm=TRUE) )
+  beta.hat = exp(-intercept/alpha.hat)
+  structure( list(shape=alpha.hat, scale=beta.hat),  class = "weibull.estimate")
 }
 #------------------------------
 
